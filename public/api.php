b8a04505e0b5e27075ce4d97da817eb47c3c12a6
Boris Markov <bomar3110@gmail.com>
2023-06-08 16:07:31 +0300

    Fixes

--- public/api.php
+++ public/api.php
@@ -1,23 +1,26 @@
 <?php
   echo "HERE 2";
-
+  
   $headers = apache_request_headers();
   $jwt = "";
   if (isset($headers['Authorization'])) {
-      $auth_header = $headers['Authorization'];
-      $exploded = explode(" ", $auth_header);
-
-      if ($exploded[0] !== "Bearer") {
-          http_response_code(400);
-          die;
-      }
-
-      $jwt = $exploded[1];
+    $auth_header = $headers['Authorization'];
+    $exploded = explode(" ", $auth_header);
+    
+    if ($exploded[0] !== "Bearer") {
+      http_response_code(400);
+      die;
+    }
+    
+    $jwt = $exploded[1];
   }
-
+  
+  echo "HERE 3";
   $endpoint = $_GET['endpoint'];
   $data = json_decode(file_get_contents("php://input"), true);
-
-
+  
+  echo "HERE 4";
+  
   include_once "../src/api/$endpoint.php";
+  echo "HERE 5";
 ?>
